39083ee66a326d305a9ce71da0002806acca3639
mixuehua <mixue602@163.com>
2017-12-22 15:38:17 +0800

    deleate

--- page/page/couponsuse/couponsuse.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './app';
-
-new Vue({
-	el: document.querySelector('.my'),
-	template: '<App/>',
-	components: { App },
-	replace: false
-})
--- page/page/nearbystores/nearbystores.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './app';
-
-let vm=new Vue({
-	el: document.querySelector('.my'),
-	template: '<App/>',
-	components: { App },
-	replace: false
-})
